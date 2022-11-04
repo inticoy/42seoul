711eb00bd8869bc5f1725f7375a00ee272ea1c6b
inticoy <inticoy0406@gmail.com>
2022-11-04 21:44:17 +0900

    libft passed excpet iter, map

--- 0_circle/libft/source/ft_lstadd_back.c
+++ 0_circle/libft/source/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 20:37:16 by gyoon             #+#    #+#             */
-/*   Updated: 2022/11/04 21:34:47 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/04 21:36:57 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-	if (!*lst)
+	else if (!*lst)
 		*lst = new;
 	else
 		ft_lstadd_back(&(*lst)->next, new);
--- 0_circle/libft/source/ft_lstclear.c
+++ 0_circle/libft/source/ft_lstclear.c
@@ -6,17 +6,19 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 20:42:07 by gyoon             #+#    #+#             */
-/*   Updated: 2022/11/04 19:15:13 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/04 21:42:33 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_lstclear(t_list **lst, void (*del) (void *))
-// {
-// 	t_list	**l;
-// 	void	*n;
-
-// 	l = lst;
-// 	n = del;
-// }
+void	ft_lstclear(t_list **lst, void (*del) (void *))
+{
+	if (*lst)
+	{
+		ft_lstclear(&(*lst)->next, del);
+		del((*lst)->content);
+		free(*lst);
+		*lst = 0;
+	}
+}
