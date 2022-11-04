906cabbbc72395419d5effc6f41da59fc731ba04
inticoy <inticoy0406@gmail.com>
2022-11-04 21:51:28 +0900

    libft passed except lstmap

--- 0_circle/libft/source/ft_lstiter.c
+++ 0_circle/libft/source/ft_lstiter.c
@@ -6,12 +6,17 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 20:44:36 by gyoon             #+#    #+#             */
-/*   Updated: 2022/09/14 20:48:51 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/04 21:49:41 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_lstiter(t_list *lst, void (*f) (void *))
-// {
-// }
+void	ft_lstiter(t_list *lst, void (*f) (void *))
+{
+	if (!lst)
+		return ;
+	if (lst->next)
+		ft_lstiter(lst->next, f);
+	f(lst->content);
+}
