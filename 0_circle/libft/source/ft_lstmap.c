d2ba0ba03d1c970e43916ebab3396c8fc54faa31
inticoy <inticoy0406@gmail.com>
2022-11-04 22:56:06 +0900

    libft passed bonus francinette

--- 0_circle/libft/source/ft_lstmap.c
+++ 0_circle/libft/source/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 20:45:14 by gyoon             #+#    #+#             */
-/*   Updated: 2022/11/04 21:54:30 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/04 22:54:09 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *))
 {
-	return (0);
+	const int	size = ft_lstsize(lst);
+	t_list		*ret;
+	t_list		*t;
+	int			i;
+	void		*temp;
+
+	if (!lst)
+		return (0);
+	temp = del;
+	i = 0;
+	ret = ft_lstnew(f(lst->content));
+	lst = lst->next;
+	t = ret;
+	while (i < size - 1)
+	{
+		t->next = ft_lstnew(f(lst->content));
+		t = t->next;
+		lst = lst->next;
+		i++;
+	}
+	return (ret);
 }
