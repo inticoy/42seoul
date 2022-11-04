b0789c762c249c6fa25934119712152413d4837c
inticoy <inticoy0406@gmail.com>
2022-11-04 20:27:09 +0900

    libft refactoring ft_itoa

--- 0_circle/libft/source/ft_itoa.c
+++ 0_circle/libft/source/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:23:02 by gyoon             #+#    #+#             */
-/*   Updated: 2022/11/04 20:13:07 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/04 20:26:50 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,31 +43,3 @@ char	*ft_itoa(int n)
 	set_itoa(ret + digits - 1, n);
 	return (ret);
 }
-
-// #include <stdio.h>
-// #include <limits.h>
-
-// int main(void)
-// {
-// 	printf("%s\n", ft_itoa(0));
-// 	printf("%s\n", ft_itoa(5));
-// 	printf("%s\n", ft_itoa(55));
-// 	printf("%s\n", ft_itoa(INT_MAX));
-// 	printf("%s\n", ft_itoa(0));
-// 	printf("%s\n", ft_itoa(-5));
-// 	printf("%s\n", ft_itoa(-55));
-// 	printf("%s\n", ft_itoa(INT_MIN));
-// 	return (0);
-// }
-
-// static void	set_itoa(char *s, int n)
-// {
-// 	if (10 <= n || n <= -10)
-// 		set_itoa(s, n / 10);
-// 	if (n < 0)
-// 	{
-// 		*s = '-';
-// 		*(s + get_digits(n) - 1) = '0' - (n % 10);
-// 	}
-// 	else
-// 		*(s + get_digits(n) - 1) = '0' + (n % 10);
