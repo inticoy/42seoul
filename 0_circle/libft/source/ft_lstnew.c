690dc7b9b51b84012b159ba2119b7142b7d8b521
inticoy <inticoy0406@gmail.com>
2022-11-04 22:51:13 +0900

    libft lstnew using malloc to calloc

--- 0_circle/libft/source/ft_lstnew.c
+++ 0_circle/libft/source/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 20:11:42 by gyoon             #+#    #+#             */
-/*   Updated: 2022/09/14 20:52:50 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/04 22:50:31 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*ret;
 
-	ret = (t_list *) malloc(sizeof(t_list));
+	ret = (t_list *) ft_calloc(1, sizeof(t_list));
 	if (!ret)
 		return (0);
 	ret->content = content;
-	ret->next = 0;
 	return (ret);
 }
