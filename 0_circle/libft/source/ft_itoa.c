fc9673d2eebee208ba62da74242c078a2d33a205
inticoy <inticoy0406@gmail.com>
2022-11-07 23:34:24 +0900

    libft refactoring itoa

--- 0_circle/libft/source/ft_itoa.c
+++ 0_circle/libft/source/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:23:02 by gyoon             #+#    #+#             */
-/*   Updated: 2022/11/04 20:26:50 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/07 23:33:54 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,26 @@ static size_t	get_digits(int n)
 {
 	if (0 <= n && n < 10)
 		return (1);
-	else
-		return (get_digits(n / 10) + 1);
+	return (get_digits(n / 10) + 1);
 }
 
-static void	set_itoa(char *s, int n)
+static char	*set_itoa(char *s, int n)
 {
 	if (-10 < n && n < 0)
-		*(s - 1) = '-';
-	if (n < 0)
-		*s = '0' - n % 10;
-	else
-		*s = '0' + n % 10;
-	if (10 <= n || n <= -10)
-		set_itoa(s - 1, n / 10);
+		*s++ = '-';
+	else if (n >= 10 || n <= -10)
+		s = set_itoa(s, n / 10);
+	*s = '0' + (n % 10) * ((n > 0) - (n < 0));
+	return (s + 1);
 }
 
 char	*ft_itoa(int n)
 {
-	const size_t	digits = get_digits(n);
 	char			*ret;
 
-	ret = (char *) ft_calloc(digits + 1, sizeof(char));
+	ret = (char *) ft_calloc(get_digits(n) + 1, sizeof(char));
 	if (!ret)
 		return (0);
-	set_itoa(ret + digits - 1, n);
+	set_itoa(ret, n);
 	return (ret);
 }
