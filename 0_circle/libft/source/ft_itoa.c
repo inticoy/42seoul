f36be87cd33cde621f7379c1dd565ff343009fa5
inticoy <inticoy0406@gmail.com>
2022-11-09 15:19:08 +0900

    libft itoa arranged

--- 0_circle/libft/source/ft_itoa.c
+++ 0_circle/libft/source/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:23:02 by gyoon             #+#    #+#             */
-/*   Updated: 2022/11/07 23:35:29 by gyoon            ###   ########.fr       */
+/*   Updated: 2022/11/09 15:17:56 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ static char	*set_itoa(char *s, int n)
 {
 	if (-10 < n && n < 0)
 		*s++ = '-';
-	else if (n >= 10 || n <= -10)
+	else if (10 <= n || n <= -10)
 		s = set_itoa(s, n / 10);
-	*s++ = '0' + (n % 10) * ((n > 0) - (n < 0));
+	*s++ = '0' + ((n > 0) - (n < 0)) * (n % 10);
 	return (s);
 }
 
 char	*ft_itoa(int n)
 {
-	char			*ret;
+	char	*ret;
 
 	ret = (char *) ft_calloc(get_digits(n) + 1, sizeof(char));
 	if (!ret)
