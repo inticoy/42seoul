fc34404ee356802fdc239e344b3957af22241c72
inticoy <inticoy0406@gmail.com>
2023-01-05 16:47:42 +0900

    so_long implement block but not moving although it can move

--- 2_circle/so_long/test/srcs/hook_loop.c
+++ 2_circle/so_long/test/srcs/hook_loop.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 13:49:14 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/05 15:51:30 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/05 16:45:20 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,59 @@ int	hook_loop(t_game *g)
 			g->player.a.x = 1;
 	}
 	g->player.v.x += g->player.a.x;
-	g->player.pos.x += g->player.v.x;
-	if (g->key.press_w)
-		g->player.pos.y -= 6;
+
+
+	g->player.a.y = 0;
 	if (g->key.press_s)
-		g->player.pos.y += 8;
+	{
+		if (0 <= g->player.v.y && g->player.v.y < 4 && g->frame % 2 == 0)
+			g->player.a.y = 1;
+	}
+	else
+	{
+		if (g->player.v.y > 0 && g->frame % 8 == 0)
+			g->player.a.y = -1;
+	}
+	if (g->key.press_w)
+	{
+		if (-4 < g->player.v.y && g->player.v.y <= 0 && g->frame % 2 == 0)
+			g->player.a.y = -1;
+	}
+	else
+	{
+		if (g->player.v.y < 0 && g->frame % 8 == 0)
+			g->player.a.y = 1;
+	}
+	g->player.v.y += g->player.a.y;
+	
+	int blocked = 0;
+	t_point	next;
+	next = init_point(2, g->player.pos.x + g->player.v.x, g->player.pos.y + g->player.v.y, -1);
+	if (g->map.map[next.y/32][next.x/32] == '1')
+	{
+		blocked = 1;
+		ft_printf("A\n");
+	}
+	if (g->map.map[next.y/32][(next.x + 31)/32] == '1')
+	{
+		blocked = 1;
+		ft_printf("B\n");
+	}
+	if (g->map.map[(next.y + 31)/32][next.x/32] == '1')
+	{
+		blocked = 1;
+		ft_printf("C\n");
+	}
+	if (g->map.map[(next.y + 31)/32][(next.x + 31)/32] == '1')
+	{
+		blocked = 1;
+		ft_printf("D\n");
+	}
+	if (!blocked)
+	{
+		g->player.pos.x += g->player.v.x;
+		g->player.pos.y += g->player.v.y;
+	}
 	draw_game(*g);
 	return (0);
 }
