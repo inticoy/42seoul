6c2e26e657833bbacc5e5adda17c8f7437dcc283
inticoy <inticoy0406@gmail.com>
2023-01-05 17:16:44 +0900

    so_long jump and blocking

--- 2_circle/so_long/test/srcs/hook_loop.c
+++ 2_circle/so_long/test/srcs/hook_loop.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 13:49:14 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/05 16:45:20 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/05 17:15:21 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,56 +43,85 @@ int	hook_loop(t_game *g)
 
 
 	g->player.a.y = 0;
-	if (g->key.press_s)
-	{
-		if (0 <= g->player.v.y && g->player.v.y < 4 && g->frame % 2 == 0)
-			g->player.a.y = 1;
-	}
-	else
-	{
-		if (g->player.v.y > 0 && g->frame % 8 == 0)
-			g->player.a.y = -1;
-	}
+	// if (g->key.press_s)
+	// {
+	// 	if (0 <= g->player.v.y && g->player.v.y < 4 && g->frame % 2 == 0)
+	// 		g->player.a.y = 1;
+	// }
+	// else
+	// {
+	// 	if (g->player.v.y > 0 && g->frame % 8 == 0)
+	// 		g->player.a.y = -1;
+	// }
 	if (g->key.press_w)
 	{
 		if (-4 < g->player.v.y && g->player.v.y <= 0 && g->frame % 2 == 0)
-			g->player.a.y = -1;
+			g->player.a.y = -4;
 	}
 	else
 	{
 		if (g->player.v.y < 0 && g->frame % 8 == 0)
-			g->player.a.y = 1;
+			g->player.a.y = -4;
 	}
+	g->player.a.y += 1;
 	g->player.v.y += g->player.a.y;
 	
-	int blocked = 0;
-	t_point	next;
-	next = init_point(2, g->player.pos.x + g->player.v.x, g->player.pos.y + g->player.v.y, -1);
-	if (g->map.map[next.y/32][next.x/32] == '1')
+	int blocked_x = 0;
+	int	blocked_y = 0;
+	t_point	next_x;
+	t_point	next_y;
+
+	next_x = init_point(2, g->player.pos.x + g->player.v.x, g->player.pos.y, -1);
+	next_y = init_point(2, g->player.pos.x, g->player.pos.y + g->player.v.y, -1);
+	if (g->map.map[next_x.y/32][next_x.x/32] == '1')
 	{
-		blocked = 1;
+		blocked_x = 1;
 		ft_printf("A\n");
 	}
-	if (g->map.map[next.y/32][(next.x + 31)/32] == '1')
+	if (g->map.map[next_x.y/32][(next_x.x + 31)/32] == '1')
 	{
-		blocked = 1;
+		blocked_x = 1;
 		ft_printf("B\n");
 	}
-	if (g->map.map[(next.y + 31)/32][next.x/32] == '1')
+	if (g->map.map[(next_x.y + 31)/32][next_x.x/32] == '1')
 	{
-		blocked = 1;
+		blocked_x = 1;
 		ft_printf("C\n");
 	}
-	if (g->map.map[(next.y + 31)/32][(next.x + 31)/32] == '1')
+	if (g->map.map[(next_x.y + 31)/32][(next_x.x + 31)/32] == '1')
 	{
-		blocked = 1;
+		blocked_x = 1;
 		ft_printf("D\n");
 	}
-	if (!blocked)
-	{
+	if (blocked_x)
+		g->player.v.x = 0;
+	else
 		g->player.pos.x += g->player.v.x;
-		g->player.pos.y += g->player.v.y;
+
+	if (g->map.map[next_y.y/32][next_y.x/32] == '1')
+	{
+		blocked_y = 1;
+		ft_printf("A\n");
 	}
+	if (g->map.map[next_y.y/32][(next_y.x + 31)/32] == '1')
+	{
+		blocked_y = 1;
+		ft_printf("B\n");
+	}
+	if (g->map.map[(next_y.y + 31)/32][next_y.x/32] == '1')
+	{
+		blocked_y = 1;
+		ft_printf("C\n");
+	}
+	if (g->map.map[(next_y.y + 31)/32][(next_y.x + 31)/32] == '1')
+	{
+		blocked_y = 1;
+		ft_printf("D\n");
+	}
+	if (blocked_y)
+		g->player.v.y = 0;
+	else
+		g->player.pos.y += g->player.v.y;
 	draw_game(*g);
 	return (0);
 }
